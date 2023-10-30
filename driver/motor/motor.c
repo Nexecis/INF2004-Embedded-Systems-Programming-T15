c87f7c4eadd980cff06291b6148bb0b113993c85
Abdul Halim <2837343A@student.gla.ac.uk>
2023-10-30 16:43:59 +0800

    Added and modified the comments.
    Comments on most of the codes functions.

--- driver/motor/motor.c
+++ driver/motor/motor.c
@@ -12,81 +12,81 @@
 
 void motor_setup() {
     // Configure GPIO pins for motor control as outputs
-    gpio_init(MOTOR_ENA);
-    gpio_init(MOTOR_IN1);
-    gpio_init(MOTOR_IN2);
+    gpio_init(MOTOR_ENA); // Initialize the motor A enable pin
+    gpio_init(MOTOR_IN1); // Initialize motor A input 1
+    gpio_init(MOTOR_IN2); // Initialize motor A input 2
 
-    gpio_init(MOTOR_ENB);
-    gpio_init(MOTOR_IN3);
-    gpio_init(MOTOR_IN4);
+    gpio_init(MOTOR_ENB); // Initialize the motor B enable pin
+    gpio_init(MOTOR_IN3); // Initialize motor B input 1
+    gpio_init(MOTOR_IN4); // Initialize motor B input 2
 
-    gpio_set_dir(MOTOR_ENA, GPIO_OUT);
-    gpio_set_dir(MOTOR_IN1, GPIO_OUT);
-    gpio_set_dir(MOTOR_IN2, GPIO_OUT);
+    gpio_set_dir(MOTOR_ENA, GPIO_OUT); // Set motor A enable pin as an output
+    gpio_set_dir(MOTOR_IN1, GPIO_OUT); // Set motor A input 1 as an output
+    gpio_set_dir(MOTOR_IN2, GPIO_OUT); // Set motor A input 2 as an output
 
-    gpio_set_dir(MOTOR_ENB, GPIO_OUT);
-    gpio_set_dir(MOTOR_IN3, GPIO_OUT);
-    gpio_set_dir(MOTOR_IN4, GPIO_OUT);
+    gpio_set_dir(MOTOR_ENB, GPIO_OUT); // Set motor B enable pin as an output
+    gpio_set_dir(MOTOR_IN3, GPIO_OUT); // Set motor B input 1 as an output
+    gpio_set_dir(MOTOR_IN4, GPIO_OUT); // Set motor B input 2 as an output
 
     // Initialize motor control (set ENA and ENB HIGH for full speed)
-    gpio_put(MOTOR_ENA, 1);
-    gpio_put(MOTOR_IN1, 0);
-    gpio_put(MOTOR_IN2, 0);
+    gpio_put(MOTOR_ENA, 1); // Enable motor A
+    gpio_put(MOTOR_IN1, 0); // Set motor A input 1 to low
+    gpio_put(MOTOR_IN2, 0); // Set motor A input 2 to low
 
-    gpio_put(MOTOR_ENB, 1);
-    gpio_put(MOTOR_IN3, 0);
-    gpio_put(MOTOR_IN4, 0);
+    gpio_put(MOTOR_ENB, 1); // Enable motor B
+    gpio_put(MOTOR_IN3, 0); // Set motor B input 1 to low
+    gpio_put(MOTOR_IN4, 0); // Set motor B input 2 to low
 }
 
 void move_motor_a_forward(uint speed) {
     // Set motor A direction for forward motion
-    gpio_put(MOTOR_IN1, 1);
-    gpio_put(MOTOR_IN2, 0);
+    gpio_put(MOTOR_IN1, 1); // Set motor A input 1 to high
+    gpio_put(MOTOR_IN2, 0); // Set motor A input 2 to low
 
     // Adjust the speed of motor A using PWM (e.g., speed 0-255)
-    pwm_set_wrap(0, 255);  // Adjust the period for motor speed control
-    pwm_set_chan_level(0, PWM_CHAN_A, speed);  // Adjust speed as needed
+    pwm_set_wrap(0, 255);  // Set the PWM period for motor speed control
+    pwm_set_chan_level(0, PWM_CHAN_A, speed);  // Set the motor A speed
 }
 
 void move_motor_a_backward(uint speed) {
     // Set motor A direction for backward motion
-    gpio_put(MOTOR_IN1, 0);
-    gpio_put(MOTOR_IN2, 1);
+    gpio_put(MOTOR_IN1, 0); // Set motor A input 1 to low
+    gpio_put(MOTOR_IN2, 1); // Set motor A input 2 to high
 
     // Adjust the speed of motor A using PWM (e.g., speed 0-255)
-    pwm_set_wrap(0, 255);  // Adjust the period for motor speed control
-    pwm_set_chan_level(0, PWM_CHAN_A, speed);  // Adjust speed as needed
+    pwm_set_wrap(0, 255);  // Set the PWM period for motor speed control
+    pwm_set_chan_level(0, PWM_CHAN_A, speed);  // Set the motor A speed
 }
 
 void move_motor_b_forward(uint speed) {
     // Set motor B direction for forward motion
-    gpio_put(MOTOR_IN3, 1);
-    gpio_put(MOTOR_IN4, 0);
+    gpio_put(MOTOR_IN3, 1); // Set motor B input 1 to high
+    gpio_put(MOTOR_IN4, 0); // Set motor B input 2 to low
 
     // Adjust the speed of motor B using PWM (e.g., speed 0-255)
-    pwm_set_wrap(1, 255);  // Adjust the period for motor speed control
-    pwm_set_chan_level(1, PWM_CHAN_A, speed);  // Adjust speed as needed
+    pwm_set_wrap(1, 255);  // Set the PWM period for motor speed control
+    pwm_set_chan_level(1, PWM_CHAN_A, speed);  // Set the motor B speed
 }
 
 void move_motor_b_backward(uint speed) {
     // Set motor B direction for backward motion
-    gpio_put(MOTOR_IN3, 0);
-    gpio_put(MOTOR_IN4, 1);
+    gpio_put(MOTOR_IN3, 0); // Set motor B input 1 to low
+    gpio_put(MOTOR_IN4, 1); // Set motor B input 2 to high
 
     // Adjust the speed of motor B using PWM (e.g., speed 0-255)
-    pwm_set_wrap(1, 255);  // Adjust the period for motor speed control
-    pwm_set_chan_level(1, PWM_CHAN_A, speed);  // Adjust speed as needed
+    pwm_set_wrap(1, 255);  // Set the PWM period for motor speed control
+    pwm_set_chan_level(1, PWM_CHAN_A, speed);  // Set the motor B speed
 }
 
 void stop() {
     // Stop both motors
-    gpio_put(MOTOR_IN1, 0);
-    gpio_put(MOTOR_IN2, 0);
-    pwm_set_chan_level(0, PWM_CHAN_A, 0);  // Set PWM duty cycle to 0
+    gpio_put(MOTOR_IN1, 0); // Set motor A input 1 to low
+    gpio_put(MOTOR_IN2, 0); // Set motor A input 2 to low
+    pwm_set_chan_level(0, PWM_CHAN_A, 0);  // Set PWM duty cycle for motor A to 0
 
-    gpio_put(MOTOR_IN3, 0);
-    gpio_put(MOTOR_IN4, 0);
-    pwm_set_chan_level(1, PWM_CHAN_A, 0);  // Set PWM duty cycle to 0
+    gpio_put(MOTOR_IN3, 0); // Set motor B input 1 to low
+    gpio_put(MOTOR_IN4, 0); // Set motor B input 2 to low
+    pwm_set_chan_level(1, PWM_CHAN_A, 0);  // Set PWM duty cycle for motor B to 0
 }
 
 int main() {
@@ -99,7 +99,7 @@ int main() {
     // Move motor B forward at a specified speed
     move_motor_b_forward(180);  // Adjust speed as needed
 
-    sleep_ms(500);  // Keep the motors running forward for 1 second
+    sleep_ms(500);  // Keep the motors running forward for 0.5 second
 
     // Stop both motors
     stop();
@@ -110,7 +110,7 @@ int main() {
     move_motor_a_backward(180);  // Adjust speed as needed
     move_motor_b_forward(180);  // Adjust speed as needed
 
-    sleep_ms(500);  // Keep the robot turning right for 1 second
+    sleep_ms(500);  // Keep the robot turning left for 0.5 second
 
     // Stop both motors
     stop();
@@ -123,7 +123,7 @@ int main() {
     // Move motor B forward at a specified speed
     move_motor_b_forward(180);  // Adjust speed as needed
 
-    sleep_ms(500);  // Keep the motors running forward for 1 second
+    sleep_ms(500);  // Keep the motors running forward for 0.5 second
 
     // Stop both motors
     stop();
@@ -134,7 +134,7 @@ int main() {
     move_motor_a_forward(180);  // Adjust speed as needed
     move_motor_b_backward(180);  // Adjust speed as needed
 
-    sleep_ms(500);  // Keep the robot turning left for 1 second
+    sleep_ms(500);  // Keep the robot turning right for 0.5 second
 
     // Stop both motors
     stop();
@@ -147,7 +147,7 @@ int main() {
     // Move motor B backward at a specified speed
     move_motor_b_backward(150);  // Adjust speed as needed
 
-    sleep_ms(500);  // Keep the motors running backward for 1 second
+    sleep_ms(500);  // Keep the motors running backward for 0.5 second
 
     // Stop both motors
     stop();
