db0ccbe4217f18b146bc06eb59af9220eaf11ec3
Nexecis <nexecis@gmail.com>
2023-11-29 12:34:38 +0800

    clean up barcode client

--- partial_integration/barcode_scanner_wifi/barcode_scanner_client.py
+++ partial_integration/barcode_scanner_wifi/barcode_scanner_client.py
@@ -1,23 +1,5 @@
 import socket
 import sys
-import platform
-
-def get_char():
-    if platform.system() == 'Windows':
-        import msvcrt
-        return msvcrt.getch().decode('utf-8')
-    else:
-        import tty
-        import sys
-
-        fileno = sys.stdin.fileno()
-        old_settings = tty.tcgetattr(fileno)
-        try:
-            tty.setraw(fileno)
-            char = sys.stdin.read(1)
-        finally:
-            tty.tcsetattr(fileno, tty.TCSADRAIN, old_settings)
-        return char
     
 # Check server ip address set
 if len(sys.argv) < 2:
@@ -29,7 +11,6 @@ SERVER_ADDR = sys.argv[1]
 # These constants should match the server
 BUF_SIZE = 1
 SERVER_PORT = 4242
-TEST_ITERATIONS = 1
 
 # Open socket to the server
 sock = socket.socket()
@@ -54,22 +35,5 @@ while(True):
         # Check size of data received
     if len(read_buf) != BUF_SIZE:
         raise RuntimeError('wrong amount of data read %d', len(read_buf))
-
-    # Send the data back to the server
-    # send_buf = b'JINGYI'
-    # write_len = sock.send(send_buf)
-    # print('written %d bytes to server' % write_len)
-    # if write_len != BUF_SIZE:
-    #     raise RuntimeError('wrong amount of data written')
-
-# while True:
-#     user_char = get_char()
-#     send_buf = user_char.encode('utf-8')
-#     write_len = sock.send(send_buf)
-#     print('written %d bytes to server' % write_len)
-#     if write_len != BUF_SIZE:
-#         raise RuntimeError('wrong amount of data written')
-#     if(send_buf == b'X'):
-#         break
 
 sock.close()
